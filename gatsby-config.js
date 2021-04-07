395ab7fd0feedf08c6fc93663f551bdaeb208cb2
weixiongBirnary <49123453+weixiongBirnary@users.noreply.github.com>
2021-04-07 11:26:01 +0800

    update trackingId

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+        trackingId: 'G-V6R5Z0GGJQ',
       },
     },
     {
